fb81988d31f235ac33453986b70728f1deb2cd22
Rohit <rohitsharma2k3@gmail.com>
2022-09-21 09:58:30 +0530

    React meme generator

---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./css/Header.css"
+
+export default function Header() {
+    return (
+        <>
+        <nav>
+            <h1 className="logo">Meme-Generator</h1>
+            <h2 className="nav-tagline">Created by Rohit</h2>
+        </nav>
+        </>
+    )
+}
